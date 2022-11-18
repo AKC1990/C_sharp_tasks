337ec5aef7dddf2a37c1e0cdcb393e60bd26eb3c
Kirill Antropov <chs4t-740@mail.ru>
2022-11-18 22:55:40 +0300

    Added Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,17 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+
+int a = 4;
+int b = 16;
+
+if (a * a == b)
+{
+    Console.Write(b);
+    Console.Write(" является квадратом числа ");
+    Console.Write(a);
+}
+else
+{
+    Console.Write(b);
+    Console.Write(" не является квадратом числа ");
+    Console.Write(a);
+}
