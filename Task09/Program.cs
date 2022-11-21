800b3801c65faed252b8a23d6e0cf5bedd6d2990
Kirill Antropov <chs4t-740@mail.ru>
2022-11-21 15:20:10 +0300

    Added Task09

---
+++ Task09/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+int number = 526;
+if (number >= 100 && number <= 999)
+{
+    Console.WriteLine(number % 10);
+}
+else
+{
+    Console.WriteLine("Error. Incorrect number");
+}
