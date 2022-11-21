981d11f9eb45d7ebe049350979d7e08d464846bc
Kirill Antropov <chs4t-740@mail.ru>
2022-11-21 16:25:30 +0300

    Added Task12

---
+++ Task12/Program.cs
@@ -0,0 +1,16 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+int number = 123;
+
+int a = number / 100;
+int b = number % 10;
+ 
+if (number >= 100 && number <= 999)
+{
+    Console.Write(a);
+    Console.Write(b);
+}
+else
+{
+    Console.WriteLine("Error. Incorrect number");
+}
