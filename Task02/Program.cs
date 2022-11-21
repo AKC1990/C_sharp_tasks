a978805609e64120a5726ba5c74f95a9ac4d1b3e
Kirill Antropov <chs4t-740@mail.ru>
2022-11-21 10:19:00 +0300

    Added Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,27 @@
+﻿// Даны два числа. Показать большее и меньшее число
+//Требуется написать код для определения наибольшего и наименьшего целых чисел.
+
+int a = 2;
+int b = 5;
+
+if (a != b)
+{
+    if (a > b)
+    {
+        Console.Write("max = ");
+        Console.WriteLine(a);
+        Console.Write("min = ");
+        Console.WriteLine(b);
+    }
+    else
+    {
+        Console.Write("max = ");
+        Console.WriteLine(b);
+        Console.Write("min = ");
+        Console.WriteLine(a);
+    }
+}
+else
+{
+    Console.Write("Equal numbers. Please, enter another numbers");
+}
