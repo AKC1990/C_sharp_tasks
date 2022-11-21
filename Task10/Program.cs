88253a0c7c5e1f31addf8cf0566bf487dad9219a
Kirill Antropov <chs4t-740@mail.ru>
2022-11-21 15:29:16 +0300

    Added Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+int number = 264;
+if (number >= 100 && number <= 999)
+{
+    Console.WriteLine(number / 10 % 10);
+}
+else
+{
+    Console.WriteLine("Error. Incorrect number");
+}
