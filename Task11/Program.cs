856b38aab454462e93a60fa861ea3aef19645d58
Kirill Antropov <chs4t-740@mail.ru>
2022-11-21 15:50:04 +0300

    Added Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,24 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int number = 35;
+
+if (number >= 10 && number <= 99)
+
+{
+    int a = number / 10;
+    int b = number % 10;
+    
+    if (a > b)
+    {
+        Console.WriteLine(a);
+    }
+
+    else
+    {
+        Console.WriteLine(b);
+    }
+}
+else
+{
+    Console.WriteLine("Error. Incorrect number");
+}
