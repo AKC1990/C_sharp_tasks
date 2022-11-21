5742b5dcec64262b045926b491017a1bdbfbbebf
Kirill Antropov <chs4t-740@mail.ru>
2022-11-21 11:23:03 +0300

    Added Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,27 @@
+﻿// Найти максимальное из трех чисел
+int a = 4;
+int b = 8;
+int c = 6;
+
+if (a > b)
+{
+    if (a > c)
+    {
+        Console.Write(a);
+    }
+    else
+    {
+        Console.Write(c);
+    }
+}
+else
+{
+    if (b > c)
+    {
+        Console.Write(b);
+    }
+    else
+    {
+        Console.Write(c);
+    }
+}
