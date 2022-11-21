210e4a13abd46dda5a5fba079344b9b0825691f4
Kirill Antropov <chs4t-740@mail.ru>
2022-11-21 17:10:53 +0300

    Added Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Выяснить, кратно ли число заданному, если нет, вывести остаток
+
+int number1 = 22;
+int number2 = 5;
+
+if (number1 % number2 != 0)
+{
+    Console.Write(number1);
+    Console.Write(" mod ");
+    Console.Write(number2);
+    Console.Write(" = ");
+    Console.Write(number1 % number2);
+}
+else
+{
+    Console.Write(number1);
+    Console.Write(" is multiply ");
+    Console.Write(number2);
+}
