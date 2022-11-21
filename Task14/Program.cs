dc60cbf938d820bcd3a889567e2657301a602efa
Kirill Antropov <chs4t-740@mail.ru>
2022-11-21 19:33:33 +0300

    Added Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+int number = 164580;
+
+if (number >= 100)
+
+{
+    while (number >= 1000)
+    {
+        number = number / 10;
+    }
+
+    Console.WriteLine(number % 10);
+}
+else
+{
+    Console.WriteLine("There is no the third number");
+}
