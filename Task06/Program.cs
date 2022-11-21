c09165c96e6c3344b5ee1648ed5ea9c3f153d4a6
Kirill Antropov <chs4t-740@mail.ru>
2022-11-21 11:48:37 +0300

    Added Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить является ли число чётным
+
+int a = 6;
+if (a % 2 == 0)
+{
+    Console.Write(a);
+    Console.Write(" is even number");
+}
+else
+{
+    Console.Write(a);
+    Console.Write(" is odd number");
+}
