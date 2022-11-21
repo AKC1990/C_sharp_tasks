238ddddf14e7cb485c55e410665986192dcbbde2
Kirill Antropov <chs4t-740@mail.ru>
2022-11-21 15:04:06 +0300

    Added Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать четные числа от 1 до N
+
+int N = 10;
+
+int i = 1;
+
+while (i <= N)
+{   
+    if (i % 2 == 0)
+    {
+        Console.Write(i);
+        Console.Write(" ");
+    }
+    i += 1;
+}
